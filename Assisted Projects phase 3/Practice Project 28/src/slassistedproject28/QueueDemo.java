8a7d16d766daf76a0f8832ff0b6907a4931b806a
Saisivakumar6311 <84634212+Saisivakumar6311@users.noreply.github.com>
2022-07-26 19:55:10 +0530

    Assisted Projects Phase 3

---
+++ Assisted Projects phase 3/Practice Project 28/src/slassistedproject28/QueueDemo.java
@@ -0,0 +1,31 @@
+package slassistedproject28;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class QueueDemo {
+	
+	public static void main(String[] args) {
+		Queue<String> locationQueue= new LinkedList<String>();
+		
+		locationQueue.add("India");
+		locationQueue.add("China");
+		locationQueue.add("Japan");
+		locationQueue.add("Russia");
+		locationQueue.add("Indonesia");
+		locationQueue.add("US");
+		
+		System.out.println("Queue is : "+locationQueue);
+		
+		
+		//find head of queue
+		System.out.println("Head of Queue: "+locationQueue.peek());
+		
+		locationQueue.remove();
+		
+		System.out.println("After Removing Head: "+locationQueue);
+		
+	}
+
+
+}
